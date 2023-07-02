224c57143f09968c5d5d19a99663e7f199cda0fa
bekeodangyeuqn <thaiduiqn@gmail.com>
2023-07-02 23:00:39 +0700

    hot fix item

--- client/src/features/gift/modals/PhanThuongDetailModal.tsx
+++ client/src/features/gift/modals/PhanThuongDetailModal.tsx
@@ -3,25 +3,27 @@ import Title from 'antd/es/typography/Title'
 import React, { FC } from 'react'
 
 interface PhanThuongDetailModalProps {
-  open: boolean
-  onOk: () => void
-  onCancel: () => void
-  phan_thuong: IPhanThuongThongKe | undefined
+    open: boolean
+    onOk: () => void
+    onCancel: () => void
+    phan_thuong: IPhanThuongThongKe | undefined
 }
 
-export const PhanThuongDetailModal: FC<PhanThuongDetailModalProps> = ({
-  open,
-  onOk,
-  onCancel,
-  phan_thuong
-}) => {
-  return (
-    <>
-      <Modal title="Chi tiết phần thưởng" open={open} onOk={onOk} onCancel={onCancel}>
-        {phan_thuong?.items.map(item => (
-          <Title key={item.id} level={5}>{`${item.name}: ${item.pivot.soLuong}`}</Title>
-        ))}
-      </Modal>
-    </>
-  )
+export const PhanThuongDetailModal: FC<PhanThuongDetailModalProps> = ({ open, onOk, onCancel, phan_thuong }) => {
+    const capHoc = phan_thuong?.capHocType
+    const thanhTichHocTap = phan_thuong?.thanhTichHocTap
+    return (
+        <>
+            <Modal title="Chi tiết phần thưởng" open={open} onOk={onOk} onCancel={onCancel}>
+                <p>{`Cấp học: ${thanhTichHocTap === 1 ? "Tiểu học" : thanhTichHocTap === 2 ? "Trung học" : thanhTichHocTap === 3 ? "Cấp 3" : "Không có thành tích"}`}</p>
+                <p>{`Thành tích học tập: ${capHoc === 1 ? "Tiên tiến" : capHoc === 2 ? "Giỏi" : capHoc === 3 ? "Khác" : "Không có thành tích"}`}</p>
+                <Title level={5}>Các phần quà được nhận: </Title>
+                {phan_thuong?.items.map((item) => (
+                    <>
+                        <p>{`${item.name}: ${item.pivot.soLuong}`}</p>
+                    </>
+                ))}
+            </Modal>
+        </>
+    );
 }
